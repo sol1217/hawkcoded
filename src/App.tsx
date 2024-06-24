2913e409052a14e15d9588624e75b5bee9196e05
Tomás Herrera <131497649+tomasherreradev@users.noreply.github.com>
2024-06-24 01:05:06 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import { Home } from "./pages/Home/Home.tsx";
+import { Header } from "./components/layout/Header/Header.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 //services
 import Services from "./pages/Services/Services.tsx";
-import { Header } from "./components/layout/Header/Header.tsx";
+import AllServices from "./pages/Services/AllServices/AllServices.tsx";
 
 function App() {
   return (
